964044f4a0845f38c2f746d7bb218702d191c4b0
Farhad Hossain <farhadhossain0085@gmail.com>
2020-10-10 10:02:46 +0600

    models created

--- codes/models.py
+++ codes/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Language(models.Model):
+    name = models.CharField(max_length=50, null=False, blank=False)
+    language_code = models.CharField(max_length=10, null=False, blank=False)
+
+    def __str__(self):
+        return self.name
+
+
+class Code(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_utc = models.DateTimeField(auto_now_add=True)
+    last_updated_utc = models.DateTimeField(auto_now=True)
+    total_views = models.IntegerField(default=0)
+    snippet = models.TextField()
+    language = models.ForeignKey(Language, on_delete=models.SET_NULL, null=True)
+
+    def __str__(self):
+        return f"{self.language} - {self.code[:30]}"
+
+
+class Comment(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    datetime_utc = models.DateTimeField(auto_now_add=True)
+    body = models.TextField()
+    code = models.ForeignKey(Code, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.body[:30]
