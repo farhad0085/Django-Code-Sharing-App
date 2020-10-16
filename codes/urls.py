f7f4faa56aeb73f7f0738e34260a931f5d8f459a
Farhad Hossain <farhadhossain0085@gmail.com>
2020-10-16 23:10:37 +0600

    language page added

--- codes/urls.py
+++ codes/urls.py
@@ -3,5 +3,6 @@ from .views import *
 
 urlpatterns = [
     path('', home, name="home"),
+    path('lang/<language_name>/', language_page, name="language_page"),
     path('code/<code_id>/', code_view, name="code_view"),
 ]
--- codes/views.py
+++ codes/views.py
@@ -31,6 +31,28 @@ def home(request):
 
     return render(request, "codes/home.html", context)
 
+
+def language_page(request, language_name):
+
+    try:
+        language = Language.objects.get(name__iexact=language_name)
+    except:
+        response = render(request, '404.html',)
+        response.status_code = 404
+        return response
+
+    codes = Code.objects.order_by('-created_utc').filter(language__name__iexact=language_name).all()
+    per_page = 10
+    paginator = Paginator(codes, per_page).get_page(1)
+
+    context = {
+        "language": language_name,
+        "paginator": paginator
+    }
+
+    return render(request, "codes/language_page.html", context)
+
+
 def code_view(request, code_id):
     code = Code.objects.get(id=code_id)
     code.total_views = code.total_views + 1
