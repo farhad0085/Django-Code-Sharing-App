0e197057f82675017d1a8bdbfb8ac4ef41b6cc61
Farhad Hossain <farhadhossain0085@gmail.com>
2020-10-18 00:24:19 +0600

    static files

--- kodes_app/settings/production.py
+++ kodes_app/settings/production.py
@@ -1,7 +1,11 @@
 from dotenv import load_dotenv
+from pathlib import Path
+import os
 
-load_dotenv()
+ENV_PATH = os.path.join(Path(__file__).resolve().parent.parent.parent, '.env')
+
+load_dotenv(ENV_PATH)
 
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["farhad.pythonanywhere.com"]
